b943214547d0ea5c4096f774f91118b2c514b77d
kamilsmr <kamils6035@gmail.com>
2022-09-29 23:11:12 +0300

    static

--- src/blog/models.py
+++ src/blog/models.py
@@ -4,9 +4,12 @@ from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-
-
-
+    class Meta:
+        verbose_name_plural = "Categories"
+    
+    def __str__(self):
+        return self.name
+    
 class Post(models.Model):
     OPTIONS = (
         ('d', 'Draft'),
@@ -21,4 +24,10 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     status = models.CharField(max_length=10, choices=OPTIONS, default='d')
     slug = models.SlugField(blank=True, unique=True)
+
+    def __str__(self):
+        return self.title
+
+   
+
 
