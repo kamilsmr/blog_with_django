0c319de4161e9f1bb85c6b40fed016cd0c010b83
kamilsmr <kamils6035@gmail.com>
2022-09-29 22:37:22 +0300

    models

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,15 @@
+
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    image = models.ImageField()
+    # category = models.ForeignKey(category, on_delete=models.PROTECT)
+    published_date = models.DataTimeField(auto_now_add=True)
+    last_uptaded = models.DataTimeField(auto_now=True)
+    author 
+    status draft published
+    slug 
+
