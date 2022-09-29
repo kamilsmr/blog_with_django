7cadad01f03775fcb0207ea4c96a599c6fcc7fc4
kamilsmr <kamils6035@gmail.com>
2022-09-29 22:52:10 +0300

    models

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,15 +1,24 @@
-
-from unittest.util import _MAX_LENGTH
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
 
 class Post(models.Model):
+    OPTIONS = (
+        ('d', 'Draft'),
+        ('p', 'Published')
+    )
     title = models.CharField(max_length=100)
     content = models.TextField()
     image = models.ImageField()
-    # category = models.ForeignKey(category, on_delete=models.PROTECT)
-    published_date = models.DataTimeField(auto_now_add=True)
-    last_uptaded = models.DataTimeField(auto_now=True)
-    author 
-    status draft published
-    slug 
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    published_date = models.DateTimeField(auto_now_add=True)
+    last_uptaded = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=10, choices=OPTIONS, default='d')
+    slug = models.SlugField(blank=True, unique=True)
 
